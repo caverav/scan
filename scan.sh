a1e64fcbe94366e13a2f926ae9d4c12043b4ac24
K1000o <66751764+caverav@users.noreply.github.com>
2021-11-30 11:20:54 -0300

    sacar --script=vuln
    Llena mucho el output, por lo que en algunos casos puede ser molesto, para eso se puede simplemente agregar luego

--- scan.sh
+++ scan.sh
@@ -4,7 +4,7 @@ if [ $# -eq 1 ]; then
 	sudo nmap -sS --max-retries 5000 -n -Pn -p- 192.168.15.4 -oG portss
     export PUERTOS=$(cat portss | grep -o -E "([0-9])+/open" | tr -d '/open' | sed 'H;1h;$!d;x;y/\n/,/')
     echo $PUERTOS
-    sudo nmap -sCV -T4 -p$PUERTOS --script=vuln $1
+    sudo nmap -sCV -T4 -p$PUERTOS $1
 else
     echo "Uso: ./scan.sh <ip>"
 fi
